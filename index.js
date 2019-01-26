52e0aa54efccd668223a93c43fed2d1c22d14bd1
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-26 05:28:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ BoardMember.prototype.approve = function() {
   return 'You can do that!'
 }
 
-// BoardMember.prototype.doCharity = function() {
-//   return "I like to help people."
-// }
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
+}
 // //   this.releasePressStatement = function(){
 // //     return "You will see great things from Scuber."
 // //   }
