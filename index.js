783a5ee8002c6d09441edb7337c6d3de226d20be
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-26 05:28:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ BoardMember.prototype.veto = function() {
   return 'No, I must disagree';
 }
 
-// BoardMember.prototype.approve = function() {
-//   return 'You can do that!'
-// }
+BoardMember.prototype.approve = function() {
+  return 'You can do that!'
+}
 
 // BoardMember.prototype.doCharity = function() {
 //   return "I like to help people."
